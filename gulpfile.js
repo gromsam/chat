17c7b05efc6f0d12ba4625be20ff937be167167c
oleg <gromsam@mail.ru>
2018-12-28 19:46:38 +0300

    done 28_12_2018 19:46

--- gulpfile.js
+++ gulpfile.js
@@ -30,8 +30,7 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
-		'app/libs/jquery/dist/jquery.min.js', // Берем jQuery
-		'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js' // Берем Magnific Popup
+		'app/bower/jQuery/dist/jquery.min.js'
 		])
 		.pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		.pipe(uglify()) // Сжимаем JS файл
